b1f19e9e8355e14ba19549c3ee67014822a685a5
Breyson <breysonjose107@gmail.com>
2020-10-30 00:14:43 -0500

    commits js

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -63,9 +63,12 @@
 <script src="{{asset("assets/$theme/bower_components/fastclick/lib/fastclick.js")}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
+@yield("scriptsPlugins")
+    <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
+    <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+<script src="{{asset("assets/js/funciones.js")}}"></script>
+@yield("scripts")
 <!-- AdminLTE for demo purposes -->
-ç
-@yield('scripts')
 <script>
     $(function () {
       $('#example1').DataTable()
