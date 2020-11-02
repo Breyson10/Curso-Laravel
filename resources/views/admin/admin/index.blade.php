b4e300f7b6b400335e9d19b70d8ad2bccfe0f3a7
Breyson <breysonjose107@gmail.com>
2020-11-02 02:44:38 -0500

    Commits Autenticación

---
+++ resources/views/admin/admin/index.blade.php
@@ -0,0 +1,30 @@
+@extends("theme.$theme.layout")
+@section('titulo')
+Inicio-Biblioteca
+@endsection
+
+@section("styles")
+<link href="{{asset("assets/js/jquery-nestable/jquery.nestable.css")}}" rel="stylesheet" type="text/css" />
+@endsection
+
+@section("scriptsPlugins")
+<script src="{{asset("assets/js/jquery-nestable/jquery.nestable.js")}}" type="text/javascript"></script>
+@endsection
+
+@section("scripts")
+<script src="{{asset("assets/pages/scripts/admin/menu/index.js")}}" type="text/javascript"></script>
+@endsection
+
+@section('contenido')
+<div class="row">
+    <div class="col-lg-12">
+        @include('includes.mensaje')
+        <div class="box box-info">
+            <div class="box-header">
+                <h3 class="box-title">Bienvenidos</h3>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
